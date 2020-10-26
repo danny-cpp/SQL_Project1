4647c9eeb0247646435afa2402c4af190d12a42d
Danh Nguyen <huudanh2908@gmail.com>
2020-10-25 20:00:25 -0400

    structured

---
+++ Functionality/FunctionalityInterace.py
@@ -0,0 +1,40 @@
+class FunctionalityInterface:
+    # After login successfully, user will be in the navigation panel. Display all
+    # options the user has.
+    @staticmethod
+    def menuNavigate():
+        pass
+
+    # Accept string as input, return SQL string statement update. Assume that UID
+    # will be provided. It must implement a hash function to create a pid
+    @staticmethod
+    def postQuestion(uid):
+        pass
+
+    # Use all the value in the argument to create a a pid. Must be unique. Do
+    # research on this
+    @staticmethod
+    def postHash(uid, postdate, post_content):
+        pass
+
+    # Accepting keyword, order SQL query
+    @staticmethod
+    def searchPost(keyword):
+        pass
+
+    # Allows the user to choose the post by asking the pid
+    @staticmethod
+    def choosePost():
+        pass
+
+    # Post-Action-Group
+
+    # Accepting string answer, order a SQL query update
+    @staticmethod
+    def answerQuestion(pid):
+        pass
+
+    # Order an sql string to update vote according to pid
+    @staticmethod
+    def vote(pid):
+        pass
---
+++ Login/LoginInterface.py
@@ -0,0 +1,45 @@
+class LoginScreen:
+    # This method should create an interface allows the user to choose whether
+    # they are new user, or returning user. If new user, go to createNewUser().
+    # If old user, go to login
+    @staticmethod
+    def welcomeScreen():
+        pass
+
+    # This method should me static, when call create a new user, with username
+    # and password object, with a unique ID (call the hash function here). If
+    # UID already exist, throw exception
+    @staticmethod
+    def createNewUser():
+        pass
+
+    # This method should takes in 2 string argument and give a unique integer ID,
+    # this should not result in any hash collision for at least 1 million input.
+    # Try using Apache library for this one
+    @staticmethod
+    def newUserHash(username, password, city, date_of_create):
+        pass
+
+    # This method should ask the user for username and pass word. Password should be
+    # hidden.
+    @staticmethod
+    def login():
+        # Return a user object, where it has all the attributes bellow
+        pass
+
+    # Should return userID
+    def getUID(self):
+        pass
+
+    # Should return username
+    def getUsername(self):
+        pass
+
+    # Should return user password
+    def getPassword(self):
+        pass
+
+    # Should return Date of Create
+    def getCreateDate(self):
+        pass
+
