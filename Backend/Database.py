8cc4c2ff135c6d256ac15978c5330907e1def552
Danh Nguyen <huudanh2908@gmail.com>
2020-10-28 15:53:00 -0400

    (no branch): Auto stash before revert of "cm to be cherry-picked"

--- Backend/Database.py
+++ Backend/Database.py
@@ -1,6 +1,7 @@
 import sqlite3
 
 from Backend.DatabaseInterface import *
+from prettytable import PrettyTable
 
 
 # Database object control and establish, as well as terminate connection to the server.
@@ -16,6 +17,10 @@ class Database(DatabaseInterface):
     # until we define to terminate it
     def __del__(self):
         self.__conn.close()
+<<<<<<< master
+=======
+        print("Connection successfully terminated")
+>>>>>>> (no branch): Auto stash before revert of "cm to be cherry-picked"
 
     # This method accept a UID input and a user create SQL query. If the user
     # already existed, it will throw a Boolean false, else, true
@@ -24,6 +29,52 @@ class Database(DatabaseInterface):
         return True
 
     # Call backend with a SQL string
+<<<<<<< master
     def requestQuery(self, query_string):
         print(query_string)
         pass
+=======
+    def requestQuery(self, query_string, retriever=True, col_name=[],
+                     internal_call=False, debug_mode=False):
+
+        # for debugging purpose, turn this off for official version
+        if debug_mode:
+            print(query_string)
+
+        # If the query is a retriever i.e. SELECT...
+        if retriever:
+            cursor = self.__conn.execute(query_string)
+            self.__conn.commit()
+            table = PrettyTable()
+            table.field_names = col_name
+
+            # If it is not an internal call, display
+            if not internal_call:
+                for row in cursor:
+                    table.add_row(row)
+                print(table)
+
+            records = cursor.fetchall()
+            return records
+
+        else:
+            self.__conn.execute(query_string)
+            self.__conn.commit()
+
+    # Use this function to generate a new and unique post ID. Execute this method
+    # will provide a unique PID
+    def requestNewPID(self):
+        get_new_pid_SQL = "SELECT MAX(P.PID) FROM POSTS P;"
+        print(get_new_pid_SQL)
+        pid = self.requestQuery(get_new_pid_SQL, True, internal_call=True)
+        pid = pid[0][0]
+        pid = int(pid[1:]) + 100
+        pid = 'p' + str(pid)
+        return pid
+
+
+if __name__ == '__main__':
+    server = Database()
+    # server.requestQuery("SELECT * FROM users;", True)
+
+>>>>>>> (no branch): Auto stash before revert of "cm to be cherry-picked"
--- Backend/DatabaseInterface.py
+++ Backend/DatabaseInterface.py
@@ -1,15 +1,42 @@
 class DatabaseInterface:
     # This method accept a UID input and a user create SQL query. If the user
     # already existed, it will throw a Boolean false, else, true
-    @staticmethod
-    def requestUIDCheck(uid):
+    def requestUIDCheck(self, uid):
         return False
 
-    # Call backend with a SQL string
+    # Call backend with a SQL string.
+    def requestQuery(self, query_string, retriever=True, col_name=[],
+                     internal_call=False, debug_mode=False):
+        """
+        Input argument is an SQL string that will be executed onto the server
+
+        :param query_string: The input SQL string
+
+        :param retriever: If the input is a SELECT statement. i.e. "SELECT
+        * FROM ...", turning this argument to True will result in table printing,
+        True by default. For setter (UPDATE, DELETE,...), turn this arguemnt to False
+
+        :param col_name: The array of column_name
+
+        :param internal_call: Turn to True for internal methods call
+
+        :param debug_mode: Turn to True to check what the SQL is.
+
+        :return: SQLite3.Cursor object
+        """
+        pass
+
+    # Use this function to generate a new and unique post ID
     @staticmethod
-    def requestQuery(query_string):
+    def requestNewPID():
+        return "p200"
+
+    # Use this function to generate a new and unique post ID. Execute this method
+    # will provide a unique PID
+    def requestNewPID(self):
+        # Return a new and unique pid
         pass
 
 
-
-
+if __name__ == '__main__':
+    print(DatabaseInterface.requestQuery.__doc__)
