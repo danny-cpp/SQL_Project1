2da02dfb45eff316bedc4d2e8f8874008a8460b8
utkarsh-saraswat <70922561+utkarsh-saraswat@users.noreply.github.com>
2020-10-29 19:29:46 -0600

    2nd

--- Login/Display.py
+++ Login/Display.py
@@ -13,20 +13,18 @@ class Display(LoginInterface):
         print("\n\n________________________Welcome to SQLite Mini!________________________")
 
         acceptable = ['y', 'n', 'quit']
-        key = input("Are you an existing user? y / n ")
+        key = Display.prompt_user("Are you an existing user? y / n ", True)
         prompt = False
         while not prompt:
-            if key.lower() in acceptable and key.lower() == 'y':
+            if key in acceptable and key == 'y':
                 state = Display.loginScreen(svr)
                 prompt = True
-            elif key.lower() in acceptable and key.lower() == 'n':
+            elif key in acceptable and key == 'n':
                 state = Display.createNewUser(svr)
                 prompt = True
 
-            elif key.lower() == 'quit':
-                exit()
             else:
-                key = input("Wrong input, please try again. Are you an existing user? y / n ")
+                key = Display.prompt_user("Wrong input, please try again. Are you an existing user? y / n ",True)
 
         return state
 
@@ -35,17 +33,17 @@ class Display(LoginInterface):
     # UID already exist, throw exception
     @staticmethod
     def createNewUser(svr):
-        uid = input("Please enter  unique ID of your choice: ")
+        uid = Display.prompt_user("Please enter  unique ID of your choice: ", True)
         while svr.requestUIDCheck(uid):
-            uid = input("UID already taken, please input another UID: ")
-        usrname = input("Please enter your name: ")
+            uid = Display.prompt_user("UID already taken, please input another UID: ",True)
+        usrname = Display.prompt_user("Please enter your name: ", True)
         pwd = input("Please enter your password: ")
-        city = input("Please enter your city: ")
+        city = Display.prompt_user("Please enter your city: ", True)
         # Call for a new ID to be generated
         cr_date = svr.getCurrentTime()
         usr = User(uid, usrname, pwd, city, cr_date)
-        inp = input("Are you sure with this these information? If not, type 'n' to go to the Welcome Screen else Hit Enter")
-        if inp.lower() == 'n':
+        inp = Display.prompt_user("Are you sure with this these information? If not, type 'n' to go to the Welcome Screen else Hit Enter", True)
+        if inp == 'n':
             return None
         insert_user = ("INSERT INTO users (uid, name, pwd, city, crdate)" +
                        f" VALUES ('{uid}', '{usrname}', '{pwd}', '{city}', DATE('{cr_date}'));")
@@ -59,9 +57,9 @@ class Display(LoginInterface):
     @staticmethod
     def loginScreen(svr):
         # Return a user object, where it has all the attributes bellow
-        uid = input("Please enter your UID: ")
+        uid = Display.prompt_user("Please enter your UID: ", True)
         while not svr.requestUIDCheck(uid):
-            uid = input("UID have not registered, check your UID or enter 'back' to WelcomeScreen: ")
+            uid = Display.prompt_user("UID have not registered, check your UID or enter 'back' to WelcomeScreen: ", True)
             if uid == 'back':
                 return None
 
@@ -76,6 +74,15 @@ class Display(LoginInterface):
                             login_authentication[0][3], login_authentication[0][4])
 
         return current_user
+    #It will take prompt as an argument string , force_lower is true when you want to compare with lower string
+    @staticmethod
+    def prompt_user(prompt, force_lower=False):
+        user_inp = input(prompt)
+        if force_lower:
+            user_inp = user_inp.lower()
+        if user_inp == 'quit':
+            exit()
+        return user_inp
 
 
 if __name__ == '__main__':
