fce17a862272c953f5396840c0c2d3070e770bf2
Danh Nguyen <huudanh2908@gmail.com>
2020-11-01 01:19:22 -0500

    input updated

---
+++ InputManagement/InputControl.py
@@ -0,0 +1,27 @@
+from getpass import getpass
+
+
+class InputControl:
+    # Replacing input
+    @staticmethod
+    def my_input(prompt, enforce_lower=False, is_pwd=False):
+        if is_pwd:
+            user_inp = getpass(prompt)
+            return user_inp
+
+        user_inp = input(prompt)
+
+        if user_inp == 'quit':
+            exit()
+
+        if enforce_lower:
+            user_inp = user_inp.lower()
+
+
+
+        return user_inp
+
+
+if __name__ == '__main__':
+    inp = InputControl.my_input("enter: ", is_pwd=True)
+    print(inp)
